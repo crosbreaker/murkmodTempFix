93bd719b58397a631e5fd2233020c059f49d17a3
xXMariahScaryXx <mariag031809@gmail.com>
2025-09-16 16:39:54 -0500

    maybe this'll fix init??

--- chromeos_startup.sh
+++ chromeos_startup.sh
@@ -177,10 +177,10 @@ else
     if touch /root/testingForVerity; then
 	echo "verity disabled"
 	rm -rf /root/testingForVerity
+	sed -i 's/stable-channel/testimage-channel/' /etc/lsb-release
     else
 	/usr/share/vboot/bin/make_dev_ssd.sh -f --remove_rootfs_verification --partitions "$get_booted_kernnum"
    fi 
-    sed -i 's/stable-channel/testimage-channel/' /etc/lsb-release
     echo "Plugins run. Handing over to real startup..."
     if [ ! -f /new-startup ]; then
         exec /sbin/chromeos_startup.sh.old
